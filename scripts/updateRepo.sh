7d66ed82ea065cf6bb49f72706c1d8a145018d22
Borja Arias <borja.arias.upc@gmail.com>
2019-07-04 21:59:29 +0200

    CircleCI Deployment update

--- scripts/updateRepo.sh
+++ scripts/updateRepo.sh
@@ -1,7 +1,10 @@
 #!/usr/bin/env bash
 
-ssh suilabs@suilabs.com <<< "
-cd Projects/tarbean
-git pull origin master && \
+LC_PROJECT_NAME=webPlatform
+ssh -tt -o StrictHostKeyChecking=no suilabs@suilabs.com <<< "mkdir -p Projects/${LC_PROJECT_NAME}; exit" && \
+rsync -rpulz --verbose ${PWD}/../* suilabs@suilabs.com:./Projects/${LC_PROJECT_NAME}/. && \
+ssh -tt -o SendEnv=PROJECT_NAME -o StrictHostKeyChecking=no suilabs@suilabs.com <<< "
+cd Projects/${LC_PROJECT_NAME}
 htpasswd -c -b htpasswd/kae.suilabs.com $LC_NGINX_USER $LC_NGINX_PASS
-docker-compose up -d --build webPlatform"
+docker-compose up -d --build webPlatform
+exit"
